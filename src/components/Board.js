0d1747f2e3f3a1891864353cf355140c2597a3ae
ARJUN-SABU <arjunsabu99@gmail.com>
2023-06-16 17:19:33 +0530

    Added the json file with card data

---
+++ src/components/Board.js
@@ -0,0 +1,57 @@
+//styles
+import "../styles/Board.css";
+
+function Board() {
+  return (
+    <div className="board">
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board</h1>
+      <h1>I am the board 10</h1>
+    </div>
+  );
+}
+
+export default Board;
--- src/components/BoardScreen.js
+++ src/components/BoardScreen.js
@@ -1,48 +1,20 @@
 //styles
 import "../styles/BoardScreen.css";
 
+//components
+import Board from "./Board";
+
 function BoardScreen() {
   return (
     <div className="boardScreen">
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
-      <h1>I am the BoardScreen</h1>
+      <div className="boardScreen__top">
+        <h1>I am the top details</h1>
+      </div>
+      <div className="boardScreen__boardsAndCards">
+        <Board />
+        <Board />
+        <Board />
+      </div>
     </div>
   );
 }
